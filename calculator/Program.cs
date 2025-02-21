5c55d44eca36c361a1306a54cfa4c9793ac5f3b6
Jamiayn-Kh <jaik320024444@gmail.com>
2025-02-21 10:50:31 +0800

    case

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -7,14 +7,31 @@ class Program
         Calculator calc = new Calculator();
         Memory memory = new Memory();
 
-        double result1 = calc.Add(7, 2);
-        Console.WriteLine($"Add: 7 + 2 = {result1}");
-        memory.AddToMemory(result1);
+        Console.Write("ehnii toog oruul:");
+        double n1=Convert.ToDouble(Console.ReadLine());
+        Console.Write("hoyrdoh toog oruul:");
+        double n2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("(+,-) operatoriig oruul:");
+        char op=Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        
+        double result = 0;
+        switch (op)
+        {
+            case '+':
+                result=calc.Add(n1, n2);
+                Console.WriteLine($"niilber: {n1}+{n2}={result}");
+                break;
+            case '-':
+                result = calc.Subtract(n1, n2);
+                Console.WriteLine($"yalgavar: {n1}-{n2}={result}");
+                break;
+            default:
+                Console.WriteLine("buruu utga");
+                return;
 
-        double result2 = calc.Subtract(15, 5);
-        Console.WriteLine($"Subtract: 15 - 5 = {result2}");
-        memory.AddToMemory(result2);
-
+        }
+        memory.AddToMemory(result);
         memory.ShowMemory();
     }
 }
